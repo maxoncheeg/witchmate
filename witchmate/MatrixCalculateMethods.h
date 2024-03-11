3979e84b7f4410fdb9f9246646d0a8e8c14e94fb
maxoncheeg <maxoncheeg@gmail.com>
2024-03-11 19:26:24 +0700

    almost there

---
+++ witchmate/MatrixCalculateMethods.h
@@ -0,0 +1,189 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+float getMatrixNorm(vector<vector<float>> a) {
+	float sum = 0;
+	for (int i = 0; i < a.size(); i++)
+		for (int j = 0; j < a[i].size(); j++)
+			sum += pow(a[i][j], 2);
+
+	return sqrt(sum);
+}
+
+void printMatrix(vector<vector<float>>& A, vector<float>& B) {
+
+
+	for (int i = 0; i < A.size(); i++)
+	{
+		for (int j = 0; j < A[i].size(); j++)
+			printf("%.7f\t", A[i][j]);
+		printf("|\t%.7f\n", B[i]);
+	}
+	cout << endl;
+}
+
+vector<float> substractVectors(vector<float> a, vector<float> b) {
+	vector<float> x = {};
+
+	for (int i = 0; i < a.size(); i++)
+		x.push_back(a[i] - b[i]);
+
+	return x;
+}
+
+float vectorLength(vector<float> x) {
+	float sum = 0;
+	for (int i = 0; i < x.size(); i++)
+		sum += pow(x[i], 2);
+	return sqrt(sum);
+}
+
+#pragma region FIRST QUEST
+
+// matrix should be square like shit maybe, so i think i do it right
+void chooseMainElements(vector<vector<float>>& A) {
+	for (int i = 0; i < A.size(); i++)
+		for (int j = i + 1; j < A[i].size(); j++)
+			if (abs(A[i][j]) > abs(A[i][i]))
+			{
+				for (int k = 0; k < A.size(); k++)
+				{
+					float temp = A[k][i];
+					A[k][i] = A[k][j];
+					A[k][j] = temp;
+				}
+
+				continue;
+			}
+}
+
+// tests are needed
+void convertToStepwise(vector<vector<float>>& A, vector<float>& B) {
+	for (int i = 0; i < A[0].size(); i++)
+	{
+		if (A.size() <= i || A[0].size() <= i)continue;
+
+		float a = A[i][i];
+
+		if (a == 0) {
+			for (float k = 0; k < A.size(); k++)
+				if (A[k][i] != 0)
+				{
+					a = A[k][i];
+
+					A[k].swap(A[i]);
+
+					auto tempB = B[k];
+					B[k] = B[i];
+					B[i] = tempB;
+
+					break;
+				}
+
+			if (a == 0) continue;
+		}
+
+		for (int k = i + 1; k < A.size(); k++)
+		{
+			if (A[k][i] == 0) continue;
+
+			float multiplier = A[k][i] / a;
+
+			for (int j = 0; j < A[k].size(); j++)
+				A[k][j] -= A[i][j] * multiplier;
+
+			B[k] -= B[i] * multiplier;
+		}
+	}
+}
+
+vector<float> findStepwiseXs(vector<vector<float>>& A, vector<float>& B) {
+	vector<float> X = {};
+
+	for (int i = A.size() - 1; i >= 0; i--)
+	{
+		float sum = B[i];
+		for (int j = A[i].size() - 1; j >= 0; j--)
+			if (A[i][j] != 0)
+			{
+				if (X.size() >= A[i].size() - j)
+					sum -= X[A[i].size() - j - 1] * A[i][j];
+				else
+					X.push_back(sum / A[i][j]);
+			}
+			else break;
+	}
+
+	reverse(X.begin(), X.end());
+	return X;
+}
+
+#pragma endregion
+
+#pragma region SECOND QUEST
+
+void convertToSimpleIterations(vector<vector<float>>& A, vector<float>& B) {
+	for (int i = 0; i < A.size(); i++)
+	{
+		float sum = 0;
+		for (int j = 0; j < A[i].size(); j++)
+			if (j == i)continue;
+			else sum += abs(A[i][j]);
+
+		if (abs(A[i][i]) <= sum) throw new runtime_error("|A(ii)| < sum of |A(ij)|");
+	}
+
+	for (int i = 0; i < A.size(); i++)
+	{
+		float a = A[i][i];
+		B[i] /= a;
+
+		for (int j = 0; j < A.size(); j++)
+			if (i == j) A[i][j] = 0;
+			else A[i][j] /= -a;
+	}
+}
+
+vector<float> findSimpleIterationsXs(vector<vector<float>>& a, vector<float>& b, float precision) {
+	vector<float> x = b;
+	int k = 0;
+
+	float norm = getMatrixNorm(a);
+
+	if (norm >= 1)
+		throw new runtime_error("|alpha| > 1");
+
+	if (norm > 0.5)
+		precision = (1 - norm) / norm * precision;
+	float error = precision;
+	do {
+		vector<float> xk = { 0, 0, 0 };
+		for (int i = 0; i < x.size(); i++)
+		{
+			float sum = 0;
+			for (int j = 0; j < a[i].size(); j++) {
+				sum += a[i][j] * x[j];
+			}
+
+			xk[i] = b[i] + sum;
+		}
+
+		cout << "\t" << k << " iteration: ";
+		for (int k = 0; k < xk.size(); k++) printf("%.7f ", xk[k]);
+		cout << endl;
+
+		error = vectorLength(substractVectors(xk, x));
+		k++;
+		x = vector<float>(xk);
+	} while (error > precision);
+
+	cout << "Iterations amount - " << k << endl;
+
+	return x;
+}
+
+#pragma endregion
--- witchmate/witchmate.cpp
+++ witchmate/witchmate.cpp
@@ -1,6 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <Windows.h>
 
-int main(int argc, char* argv[])
-{
-    
+#include "MatrixCalculateMethods.h"
+using namespace std;
+
+void firstQuest() {
+    vector<vector<float>> A1 = {
+        { 2.00001, 2, 3, 1 },
+        { 1, 1, 1, 1 },
+        { -1, 1, -2, 1 },
+        { 2, 1, 1, 1}
+    };
+
+    auto A2 = vector<vector<float>>(A1);
+
+    vector<float> B1 = { 8.00003, 2, -1, 5 };
+    auto B2 = vector<float>(B1);
+
+    chooseMainElements(A2);
+
+    cout << "\t Matrix A1: " << endl;
+    printMatrix(A1, B1);
+
+    cout << "\t Matrix A2 with selection of major element by row: " << endl;
+    printMatrix(A2, B2);
+
+    convertToStepwise(A1, B1);
+    convertToStepwise(A2, B2);
+
+    cout << "\t Triangle matrix A1: " << endl;
+    printMatrix(A1, B1);
+
+    cout << "\t Triangle matrix A2: " << endl;
+    printMatrix(A2, B2);
+
+    auto x1 = findStepwiseXs(A1, B1);
+    auto x2 = findStepwiseXs(A2, B2);
+
+    cout << "\n\t Result A1: \t";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+    for (float x : x1)
+        cout << x << "\t\t";
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    cout << "\n\t Result A2: \t";
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    for (float x : x2)
+        cout << x << "\t\t";
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+}
+
+void secondQuest() {
+    vector<vector<float>> A = {
+        {10, 3, 1},
+        {3, 14, 2},
+        {-3, 2, 12}
+    };
+
+    vector<float> B = { 18, 35, 25 };
+
+    float precision = 1e-7;
+
+    printMatrix(A, B);
+
+    convertToSimpleIterations(A, B);
+    printMatrix(A, B);
+
+    auto x = findSimpleIterationsXs(A, B, precision);
+    for (int i = 0; i < x.size(); i++)
+        cout << x[i] << "\t";
+}
+
+#pragma endregion
+
+int main() {
+    setlocale(LC_ALL, "");
+    //firstQuest();
+    secondQuest();
+
     return 0;
 }
